e33ef4a5d17a809715348b1b527a05eaa82f67a6
abhaypithadiya <abhaypithadiya@gmail.com>
2025-03-14 18:45:01 +0530

    Feat: Show like count.

--- resources/views/livewire/post/item.blade.php
+++ resources/views/livewire/post/item.blade.php
@@ -135,7 +135,9 @@
             </div>
 
             {{-- likes and views --}}
-            <p class="font-bold text-sm">1234 likes</p>
+            @if ($post->totalLikers>0 && !$post->hide_like_view)
+            <p class="font-bold text-sm">{{$post->totalLikers}} {{$post->totalLikers>1? 'likes':'like'}}</p>
+            @endif
 
             {{-- name and comment --}}
             <div class="flex text-sm gap-2 font-medium">
